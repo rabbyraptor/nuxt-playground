f2dc1be4c3df7ee4de212bd09c95435185980c28
Mathias Hammer <>
2019-09-12 08:43:27 +0200

    Added local serverless functions
    Added netlify-lambda to run Serverless functions locally.
    Moved existing functions from /functions to /functions-source.

--- functions-source/dadjoke.js
+++ functions-source/dadjoke.js
@@ -1,5 +1,5 @@
-//import fetch from "node-fetch";
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
+//const fetch = require("node-fetch");
 
 const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
