24684ca9f33636312d3c0bafa0d7eef290d3a2de
Mathias Hammer <>
2019-09-19 09:38:43 +0200

    Added 404 error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Go back to Home</nuxt-link>
+    <img src="~/assets/img/fogg-page-not-found-1.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"]
+  //layout: 'default'
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 2em 0 0 0;
+}
+</style>
