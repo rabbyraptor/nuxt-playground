efce89b389e6f1416ae03a601218917685eeb18f
Mathias Hammer <>
2019-09-19 09:36:51 +0200

    Optimized completion of todos

--- components/todoItems.vue
+++ components/todoItems.vue
@@ -1,8 +1,11 @@
 <template>
   <ul>
-    <draggable v-model="mutableList" @start="drag=true" @end="drag=false;emitUpdateListOrder(mutableList);">
-      <li v-for="item in mutableList" :key="item.name">
-        <span class="name">{{ item.name }}</span>
+    <draggable v-model="mutableList" @start="drag=true" @end="emitUpdateListOrder">
+      <li v-for="item in mutableList" :key="item.name" :class="{'is-completed':item.completed}">
+        <div>
+          <span @click="emitCompleteItem(item)" class="complete-item"></span>
+          <span class="todo-name">{{ item.name }}</span>
+        </div>
         <span v-if="switchState" @click="emitDeleteItem(item)" class="remove-item"></span>
       </li>
     </draggable>
@@ -10,56 +13,91 @@
 </template>
 
 <script>
-export default{
-  data(){
+export default {
+  data() {
     return {
-        mutableList: this.items
-      }
+      mutableList: this.items
+    };
   },
-  props:{
-    items:{},
-    switchState:false
+  props: {
+    items: {},
+    switchState: false
   },
-  methods:{
-    emitDeleteItem(item){
+  methods: {
+    emitDeleteItem(item) {
       this.$emit("emitDeleteItem", item);
-      console.log('Item ' + item.name + ' was deleted');
+      console.log("Item " + item.name + " was deleted");
     },
-    emitUpdateListOrder(mutableList){
-      this.$emit("emitUpdateListOrder", mutableList);
+    emitUpdateListOrder() {
+      this.drag = false;
+      this.$emit("emitUpdateListOrder", this.mutableList);
+    },
+    emitCompleteItem(item) {
+      this.$emit("emitCompleteItem", item);
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  li{
-    background-color:#efefef;
-    padding:1em 2em;
-    box-sizing: border-box;
-    margin:1em 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border:1px solid #cecece;
-    cursor:move;
-    cursor: grab;
-  }
-  li:active{
-    cursor: grabbing;
-  }
-  .name{
-    padding: 3px 0;
-  }
-  .remove-item{
-    background-color:lightcoral;
-    padding: 3px 8px;
-    color:#fff;
-  }
-  .remove-item::before{
-    content: 'x';
-  }
-   .remove-item:hover{
-     cursor:pointer;
-   }
+li {
+  background-color: #efefef;
+  padding: 1em 2em;
+  box-sizing: border-box;
+  margin: 1em 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-items: start;
+  border: 1px solid #cecece;
+  cursor: move;
+  cursor: grab;
+}
+li:active {
+  cursor: grabbing;
+}
+li > div {
+  display: flex;
+}
+.todo-name {
+  padding: 3px 0;
+  float: left;
+}
+.complete-item {
+  background-color: #fff;
+  padding: 3px 6px;
+  color: #ccc;
+  float: left;
+  margin-right: 2em;
+}
+.complete-item::before {
+  content: "\2714";
+  display: inline;
+}
+.complete-item:hover {
+  cursor: pointer;
+  color: #777;
+}
+.is-completed {
+  background-color: lightgreen;
+}
+.is-completed .todo-name {
+  text-decoration: line-through;
+  color: #aaa;
+}
+.is-completed .complete-item {
+  color: green;
+}
+.remove-item {
+  background-color: lightcoral;
+  padding: 3px 8px;
+  color: #fff;
+  justify-self: end;
+}
+.remove-item::before {
+  content: "x";
+}
+.remove-item:hover {
+  cursor: pointer;
+}
 </style>
--- pages/todo/index.vue
+++ pages/todo/index.vue
@@ -4,67 +4,88 @@
     <toggle-switch class="switch-delete" @emitToggleSwitch="toggleSwitch">Delete Todos:</toggle-switch>
     <div id="todo-input">
       <h3 for="name">Create todo</h3>
-      <todo-item-add @emitAddItem="addItem" @emitTemporaryInput="saveTemporaryInput" :tempInput="tempInput"></todo-item-add>
+      <todo-item-add
+        @emitAddItem="addItem"
+        @emitTemporaryInput="saveTemporaryInput"
+        :tempInput="tempInput"
+      ></todo-item-add>
     </div>
     <div id="todo-list">
       <h2>Todo list:</h2>
       <p style="width:100%;">You have {{ items.length }} todos in your list.</p>
-      <todo-items :items="items" @emitDeleteItem="deleteItem" @emitUpdateListOrder="updateListOrder" v-if="this.items.length > 0" :switchState="switchState"></todo-items>
-      <p v-if="!containsItems">You've done all of your todos! <b>Now add some!</b></p>
+      <todo-items
+        :items="items"
+        @emitDeleteItem="deleteItem"
+        @emitUpdateListOrder="updateListOrder"
+        @emitCompleteItem="completeItem"
+        v-if="this.items.length > 0"
+        :switchState="switchState"
+      ></todo-items>
+      <p v-if="!containsItems">
+        You've done all of your todos!
+        <b>Now add some!</b>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-import todoItems from '../../components/todoItems'
-import todoItemAdd from '../../components/todoItemAdd'
-import toggleSwitch from '../../components/toggleSwitch'
-import Cookies from '../../node_modules/js-cookie/src/js.cookie.js'
+import todoItems from "../../components/todoItems";
+import todoItemAdd from "../../components/todoItemAdd";
+import toggleSwitch from "../../components/toggleSwitch";
+import Cookies from "../../node_modules/js-cookie/src/js.cookie.js";
 
 export default {
-  data(){
+  data() {
     return {
-      items:[],
-      tempInput:'',
-      switchState:false
+      items: [],
+      tempInput: "",
+      switchState: false
+    };
+  },
+  mounted() {
+    if (Cookies.get("todos")) {
+      this.items = Cookies.getJSON("todos");
     }
+    this.tempInput = Cookies.getJSON("temporaryInput", this.tempInput);
   },
-  mounted(){
-    if(Cookies.get('todos')){
-      this.items = Cookies.getJSON('todos');
-    }
-    this.tempInput = Cookies.getJSON('temporaryInput', this.tempInput)
+  updated() {
+    Cookies.set("todos", this.items);
   },
-  updated(){
-    Cookies.set('todos', this.items);
-  },
-  components:{
+  components: {
     todoItems,
     todoItemAdd,
     toggleSwitch
   },
-  methods:{
-    addItem(input){
-      this.items.push({name: input});
+  methods: {
+    addItem(input) {
+      this.items.push({ name: input });
     },
-    deleteItem(item){
+    deleteItem(item) {
       this.items.splice(this.items.indexOf(item), 1);
     },
-    saveTemporaryInput(input){
-      Cookies.set('temporaryInput', input);
-    }, 
-    toggleSwitch(switchState){
+    completeItem(item) {
+      let index = this.items.indexOf(item);
+      if (index > -1) {
+        item.completed = !item.completed;
+        this.$set(this.items, index, item);
+      }
+    },
+    saveTemporaryInput(input) {
+      Cookies.set("temporaryInput", input);
+    },
+    toggleSwitch(switchState) {
       this.switchState = switchState;
     },
-    updateListOrder(mutableList){
+    updateListOrder(mutableList) {
       this.items = mutableList;
     }
   },
-  computed:{
-    containsItems(){
-      if(this.items.length > 0){
+  computed: {
+    containsItems() {
+      if (this.items.length > 0) {
         return true;
-      }else{
+      } else {
         return false;
       }
     }
@@ -76,8 +97,8 @@ export default {
 body {
   background-color: #35495e;
 }
-h1{
-  margin-bottom:1em;
+h1 {
+  margin-bottom: 1em;
   font-size: 3em;
 }
 .wrapper {
@@ -89,33 +110,34 @@ h1{
   padding: 5%;
   background: #fff;
 }
-#todo-input{
+#todo-input {
   margin: 0 0 2em 0;
   display: flex;
   flex-flow: column wrap;
-  max-width:50%;
+  max-width: 50%;
 }
-#todo-input input, #todo-input button{
-  height:50px;
+#todo-input input,
+#todo-input button {
+  height: 50px;
   font-size: 1em;
 }
-#todo-list ul{
-  padding:0;
+#todo-list ul {
+  padding: 0;
 }
-#todo-list p{
-  margin:.5em 0 0 0;
+#todo-list p {
+  margin: 0.5em 0 0 0;
 }
-.switch-delete{
+.switch-delete {
   display: flex;
   flex-flow: column;
   justify-content: flex-end;
-  width:100%;
-  margin:0 0 1em 0;
+  width: 100%;
+  margin: 0 0 1em 0;
 }
-.switch-delete div{
-  float:left;
-  clear:both;
-  display:block;
-  height:20px;
+.switch-delete div {
+  float: left;
+  clear: both;
+  display: block;
+  height: 20px;
 }
 </style>
